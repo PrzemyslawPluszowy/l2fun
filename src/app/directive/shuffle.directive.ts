09c47d5de184242665dee46661101d0cf6e43fd2
Przemyslaw <misiek440@gmail.com>
2022-12-05 20:21:12 +0100

    next futures on shuffle directive, need more fixes

--- src/app/directive/shuffle.directive.ts
+++ src/app/directive/shuffle.directive.ts
@@ -12,18 +12,19 @@ import {
   selector: '[appShuffle]',
 })
 export class ShuffleDirective {
-  constructor(private elRef: ElementRef) {
-    this.elRef.nativeElement.style.cssText = `
-      top: ${this.shufflePositionTop}px;
-    filter: grayscale(1);
-      
-    `;
+  constructor(private elRef: ElementRef, private render: Renderer2) {
+    // this.elRef.nativeElement.style.cssText = `
+    //   top: ${this.shufflePositionTop}px;
+    // filter: grayscale(1);
+    // `;
+    // this.addModal();
+    this.createElmToAnimation();
   }
   @HostListener('click') click() {
-    this.reset();
+    // this.reset();
     this.startShuffle();
-    this.heightSingleCard = this.elRef.nativeElement.height / this.amountCard;
-    this.heightAllCard = parseInt(this.elRef.nativeElement.height);
+    this.heightSingleCard = this.elRef.nativeElement.clientHeight;
+    this.heightAllCard = this.elRef.nativeElement.nextSibling.height;
   }
 
   //config
@@ -34,12 +35,29 @@ export class ShuffleDirective {
   shufleInterval: any;
   slowDownInterval: any;
   checkPosition: any;
-
+  img: any;
   heightSingleCard: number = 0;
   heightAllCard = parseInt(this.elRef.nativeElement.height);
-  shufflePositionTop: number = 0;
 
+  shufflePositionTop: number = 0;
   randomCard = 0;
+
+  // addModal() {
+  //   const div = this.render.createElement('div');
+  //   div.classList.add('active');
+  //   this.render.appendChild(this.elRef.nativeElement.nextSibling.parentElement, div);
+  // }
+
+  createElmToAnimation() {
+    this.img = this.render.createElement('img');
+    this.render.setAttribute(
+      this.img,
+      'src',
+      '../../../assets/img/shuffle/shuffle.jpg'
+    );
+    this.render.appendChild(this.elRef.nativeElement.parentElement, this.img);
+  }
+
   reset() {
     clearInterval(this.shufleInterval);
     clearInterval(this.checkPosition);
@@ -68,10 +86,10 @@ export class ShuffleDirective {
         filter: grayscale(0);
     
     `;
-        console.log(this.shufflePositionTop);
 
         clearInterval(this.shufleInterval);
         clearInterval(this.checkPosition);
+        requestAnimationFrame(this.stopShuffle);
         console.log('stop');
       }
     }, 1);
@@ -83,10 +101,11 @@ export class ShuffleDirective {
     this.randomCard =
       0 -
       Math.ceil(heightRandom / this.heightSingleCard) * this.heightSingleCard;
-    console.log(heightRandom);
+    console.log(this.randomCard);
   }
 
   startShuffle() {
+    // requestAnimationFrame(this.startShuffle);
     setTimeout(() => {
       this.slowDownInterval = setInterval(() => {
         this.speedShuffle--;
@@ -100,7 +119,7 @@ export class ShuffleDirective {
 
     this.shufleInterval = setInterval(() => {
       this.shufflePositionTop = this.shufflePositionTop - this.speedShuffle;
-      this.elRef.nativeElement.style.cssText = `
+      this.elRef.nativeElement.nextSibling.style.cssText = `
       top: ${this.shufflePositionTop}px;
     filter: blur(2px);
     filter: grayscale(0.2);
@@ -108,7 +127,7 @@ export class ShuffleDirective {
     `;
       if (
         this.shufflePositionTop <=
-        -(this.elRef.nativeElement.height - this.heightSingleCard)
+        -(this.elRef.nativeElement.nextSibling.height - this.heightSingleCard)
       ) {
         this.shufflePositionTop = 0;
       }
