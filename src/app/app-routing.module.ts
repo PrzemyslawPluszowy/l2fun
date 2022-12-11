59f4caa06b616a8da5455c5d8488ec8846df9fcf
Przemyslaw <misiek440@gmail.com>
2022-12-11 16:59:29 +0100

    hot fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -13,6 +13,7 @@ const routes: Routes = [
   { path: 'members', component: MembersComponent },
   { path: 'info', component: SpotComponent },
   { path: 'TodayGame', component: TodayGameComponent },
+  { path: '**', component: MainComponent },
 ];
 
 @NgModule({
