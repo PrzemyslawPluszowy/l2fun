e879dfa007da95c5af162e916932291ecc61574a
Przemyslaw <misiek440@gmail.com>
2022-12-11 18:12:41 +0100

    404 add

--- src/app/page-notfound-component/page-notfound-component.component.ts
+++ src/app/page-notfound-component/page-notfound-component.component.ts
@@ -1,15 +1,34 @@
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-page-notfound-component',
   templateUrl: './page-notfound-component.component.html',
-  styleUrls: ['./page-notfound-component.component.scss']
+  styleUrls: ['./page-notfound-component.component.scss'],
+  animations: [
+    trigger('enterTrigger', [
+      state(
+        'fadeIn',
+        style({
+          opacity: '1',
+          transform: 'rotateY(0deg)',
+        })
+      ),
+      transition('void => *', [
+        style({ opacity: '50%', transform: 'rotateY(90deg)' }),
+        animate('1000ms'),
+      ]),
+    ]),
+  ],
 })
 export class PageNotfoundComponentComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
